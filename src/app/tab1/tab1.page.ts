32235c1ee9b0f1c33b34d7265139cad67bfe8741
Manuel Almejo <jomamartinezal@ittepic.edu.mx>
2021-01-18 00:50:53 -0700

    Second Challenge: last app in tabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
+
+
+import { Estudiante} from '../models/estudiante';
+import {EstudianteService} from '../services/estudiante.service';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +13,39 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public myForm: FormGroup;
+  public student: Estudiante;
+
+  constructor(private studentService: EstudianteService, private fb: FormBuilder) {
+  }
+
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: new FormControl('',
+          [Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(150)]),
+      controlnumber: new FormControl('',
+          [Validators.required,
+            Validators.minLength(10), Validators.maxLength(10)]),
+      curp: new FormControl('', [Validators.required]),
+      age: new FormControl(0, [Validators.required]),
+      active: new FormControl(false, [Validators.required])
+    });
+  }
+
+
+  create() {
+    this.student = {
+      name: this.myForm.controls.name.value,
+      controlnumber: this.myForm.controls.controlnumber.value,
+      age: this.myForm.controls.age.value,
+      curp: this.myForm.controls.curp.value,
+      active: this.myForm.controls.active.value
+    };
+
+    this.studentService.createStudent(this.student);
+  }
 
 }
